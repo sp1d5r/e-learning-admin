5d58e00afb1f810e747bf96b0f944da2f5cdd864
Elijah <aa1719@imperial.ac.uk>
2022-11-08 23:26:28 +0000

    Frontend: set up for nightmode.

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -3,6 +3,7 @@ import "./landing.css";
 import {getCourses} from "../../cloud-infrastructure/firebase";
 import CourseCard from "./course-card/course-card";
 import {useState, useEffect} from "react";
+import MiniGameCard from "./minigame-card/minigame-card";
 
 function Landing() {
     const [courses, setCourses] = useState([]);
@@ -13,6 +14,29 @@ function Landing() {
             setCourses(_courses);
             setLoading(false);
         });
+    };
+
+    const minigameItems = () => {
+        return [
+            {
+                imagePath: require("../../assets/minigame-notational/minigame1.png"),
+                title: "First Impressions",
+                time: 30,
+                difficulty: 0,
+            },
+            {
+                imagePath: require("../../assets/minigame-notational/minigame2.png"),
+                title: "Catch a Liar",
+                time: 30,
+                difficulty: 1,
+            },
+            {
+                imagePath: require("../../assets/minigame-notational/minigame3.png"),
+                title: "Tracking Client Emotions",
+                time: 30,
+                difficulty: 2,
+            },
+        ];
     };
 
     useEffect(() => {
@@ -61,6 +85,18 @@ function Landing() {
                 <div/>
             </div>
 
+            <div className={"minigames"}>
+                {minigameItems().map((item, index) => {
+                    return (
+                        <MiniGameCard
+                            imagePath={item.imagePath}
+                            title={item.title}
+                            time={item.time}
+                            difficulty={item.difficulty}
+                        />
+                    );
+                })}
+            </div>
 
         </div>
     )
---
+++ src/components/landing/minigame-card/minigame-card.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./minigame-card.css";
+import { Link } from "react-router-dom";
+
+function MiniGameCard({ imagePath, title, time, difficulty }) {
+  const _get_difficulty = () => {
+    if (difficulty === 0) {
+      return require("../../../assets/difficulty/easy.svg").default;
+    } else if (difficulty === 1) {
+      return require("../../../assets/difficulty/medium.svg").default;
+    } else {
+      return require("../../../assets/difficulty/hard.svg").default;
+    }
+  };
+
+  const _get_difficulty_name = () => {
+    if (difficulty === 0) {
+      return "easy";
+    } else if (difficulty === 1) {
+      return "medium";
+    } else {
+      return "hard";
+    }
+  };
+
+  return (
+    <Link className={"academy-content-minigame"} to={`/minigame/${title}`}>
+      <div className={"academy-content-minigame-image"}>
+        <img
+          className={"academy-content-minigame-image-data"}
+          src={imagePath}
+          alt={"minigame Notational Data 1"}
+        />
+      </div>
+      <div className={"academy-content-minigame-title"}>
+        <p>{title}</p>
+      </div>
+      <div className={"academy-content-course-info"}>
+        <img
+          src={require("../../../assets/time.png")}
+          alt={"This course is expected to take 30 minutes"}
+        />
+        <p>{time} minutes</p>
+      </div>
+      <div className={"academy-content-minigame-info"}>
+        <img
+          src={_get_difficulty()}
+          alt={"This minigame is expected to take 30 minutes"}
+        />
+        <p>{_get_difficulty_name()}</p>
+      </div>
+    </Link>
+  );
+}
+
+export default MiniGameCard;
