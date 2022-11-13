7df7a8768bf60fe075cebe253fbf2196fd30f74d
Elijah <aa1719@imperial.ac.uk>
2022-11-13 13:50:32 +0000

    Fronend: Added minigame route.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Landing from "./components/landing/landing";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CourseLanding from "./components/course-landing/course-landing";
+import Minigame from "./components/minigame-landing/minigame";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             <NavigationBar />
             <Routes>
                 <Route path="/course/" element={<CourseLanding />} />
+                <Route path="/minigame/Catch%20a%20Liar" element={<Minigame />} exact/>
                 <Route path="/" element={<Landing />} />
             </Routes>
         </BrowserRouter>
---
+++ src/components/minigame-landing/minigame.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Minigame() {
+    return <></>
+}
+
+export default Minigame;
